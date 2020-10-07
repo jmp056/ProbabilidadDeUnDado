92dc91ee04f416d31a02228759936e3302fd4526
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-10-06 23:47:27 -0400

    Logica terminada

---
+++ ProbabilidadDeUnDado/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ProbabilidadDeUnDado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.LanzarButton = new System.Windows.Forms.Button();
+            this.ResultadoLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NumericUpDown1
+            // 
+            this.NumericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumericUpDown1.Location = new System.Drawing.Point(61, 31);
+            this.NumericUpDown1.Name = "NumericUpDown1";
+            this.NumericUpDown1.Size = new System.Drawing.Size(60, 22);
+            this.NumericUpDown1.TabIndex = 0;
+            // 
+            // NumericUpDown2
+            // 
+            this.NumericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumericUpDown2.Location = new System.Drawing.Point(61, 72);
+            this.NumericUpDown2.Name = "NumericUpDown2";
+            this.NumericUpDown2.Size = new System.Drawing.Size(60, 22);
+            this.NumericUpDown2.TabIndex = 1;
+            // 
+            // NumericUpDown3
+            // 
+            this.NumericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumericUpDown3.Location = new System.Drawing.Point(61, 113);
+            this.NumericUpDown3.Name = "NumericUpDown3";
+            this.NumericUpDown3.Size = new System.Drawing.Size(60, 22);
+            this.NumericUpDown3.TabIndex = 2;
+            // 
+            // NumericUpDown4
+            // 
+            this.NumericUpDown4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumericUpDown4.Location = new System.Drawing.Point(61, 159);
+            this.NumericUpDown4.Name = "NumericUpDown4";
+            this.NumericUpDown4.Size = new System.Drawing.Size(60, 22);
+            this.NumericUpDown4.TabIndex = 3;
+            // 
+            // NumericUpDown5
+            // 
+            this.NumericUpDown5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumericUpDown5.Location = new System.Drawing.Point(61, 207);
+            this.NumericUpDown5.Name = "NumericUpDown5";
+            this.NumericUpDown5.Size = new System.Drawing.Size(60, 22);
+            this.NumericUpDown5.TabIndex = 4;
+            // 
+            // NumericUpDown6
+            // 
+            this.NumericUpDown6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumericUpDown6.Location = new System.Drawing.Point(61, 252);
+            this.NumericUpDown6.Name = "NumericUpDown6";
+            this.NumericUpDown6.Size = new System.Drawing.Size(60, 22);
+            this.NumericUpDown6.TabIndex = 5;
+            // 
+            // LanzarButton
+            // 
+            this.LanzarButton.Location = new System.Drawing.Point(327, 171);
+            this.LanzarButton.Name = "LanzarButton";
+            this.LanzarButton.Size = new System.Drawing.Size(75, 23);
+            this.LanzarButton.TabIndex = 6;
+            this.LanzarButton.Text = "Lanzar";
+            this.LanzarButton.UseVisualStyleBackColor = true;
+            this.LanzarButton.Click += new System.EventHandler(this.LanzarButton_Click);
+            // 
+            // ResultadoLabel
+            // 
+            this.ResultadoLabel.AutoSize = true;
+            this.ResultadoLabel.Location = new System.Drawing.Point(337, 99);
+            this.ResultadoLabel.Name = "ResultadoLabel";
+            this.ResultadoLabel.Size = new System.Drawing.Size(55, 13);
+            this.ResultadoLabel.TabIndex = 7;
+            this.ResultadoLabel.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultadoLabel);
+            this.Controls.Add(this.LanzarButton);
+            this.Controls.Add(this.NumericUpDown6);
+            this.Controls.Add(this.NumericUpDown5);
+            this.Controls.Add(this.NumericUpDown4);
+            this.Controls.Add(this.NumericUpDown3);
+            this.Controls.Add(this.NumericUpDown2);
+            this.Controls.Add(this.NumericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown NumericUpDown1;
+        private System.Windows.Forms.NumericUpDown NumericUpDown2;
+        private System.Windows.Forms.NumericUpDown NumericUpDown3;
+        private System.Windows.Forms.NumericUpDown NumericUpDown4;
+        private System.Windows.Forms.NumericUpDown NumericUpDown5;
+        private System.Windows.Forms.NumericUpDown NumericUpDown6;
+        private System.Windows.Forms.Button LanzarButton;
+        private System.Windows.Forms.Label ResultadoLabel;
+    }
+}
+
---
+++ ProbabilidadDeUnDado/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProbabilidadDeUnDado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LanzarButton_Click(object sender, EventArgs e)
+        {
+            LanzarDado();
+        }
+
+        public void LanzarDado()
+        {
+            int uno, dos, tres, cuatro, cinco, seis, numero;
+
+            uno = Convert.ToInt32(NumericUpDown1.Value);
+            dos = uno + Convert.ToInt32(NumericUpDown2.Value);
+            tres = dos + Convert.ToInt32(NumericUpDown3.Value);
+            cuatro = tres + Convert.ToInt32(NumericUpDown4.Value);
+            cinco = cuatro + Convert.ToInt32(NumericUpDown5.Value);
+            seis = cinco + Convert.ToInt32(NumericUpDown6.Value);
+
+            Random random = new Random();
+            numero = random.Next(0, 100);
+            ResultadoLabel.Text = Convert.ToString(numero);
+
+            if (numero <= uno)
+                ResultadoLabel.Text = "1";
+            else if (numero > uno && numero <= dos)
+                ResultadoLabel.Text = "2";
+            else if (numero > dos && numero <= tres)
+                ResultadoLabel.Text = "3";
+            else if (numero > tres && numero <= cuatro)
+                ResultadoLabel.Text = "4";
+            else if (numero > cuatro && numero <= cinco)
+                ResultadoLabel.Text = "5";
+            else
+                ResultadoLabel.Text = "6";
+        }
+    }
+}
